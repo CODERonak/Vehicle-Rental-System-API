d0f4d1cfd735260711f9b2a35f75cc8d4cc02ab3
r9988.2588 <r9988.2588@gmail.com>
2025-07-15 03:53:58 +0000

    first commit

---
+++ src/main/java/com/project/VehicleRentalSystemAPI/VehicleRentalSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.VehicleRentalSystemAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRentalSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRentalSystemApiApplication.class, args);
+	}
+
+}
