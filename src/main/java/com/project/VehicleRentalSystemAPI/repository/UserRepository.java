3a6f80151d9e3258ca825da9a7dc14caacf13b13
r9988.2588 <r9988.2588@gmail.com>
2025-07-16 08:05:26 +0000

    added few methods

--- src/main/java/com/project/VehicleRentalSystemAPI/repository/UserRepository.java
+++ src/main/java/com/project/VehicleRentalSystemAPI/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.project.VehicleRentalSystemAPI.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +10,11 @@ import com.project.VehicleRentalSystemAPI.model.entity.Users;
 @Repository // this marks this class as a repo for database interaction
 public interface UserRepository extends JpaRepository<Users, Long> {
     // These methods helps to find user with the email and phone number
-    Users findByEmail(String email);
+    Optional<Users> findByEmail(String email);
 
-    Users findByPhoneNumber(Long phoneNumber);
+    Optional<Users> findByPhoneNumber(Long phoneNumber);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByPhoneNumber(Long phoneNumber);
 }
