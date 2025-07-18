e186874c9e82e87b423967a2b6ab83e1c2dcb057
r9988.2588 <r9988.2588@gmail.com>
2025-07-18 08:21:34 +0000

    added two exceptions email and phonenumber

--- src/main/java/com/project/VehicleRentalSystemAPI/service/UserService.java
+++ src/main/java/com/project/VehicleRentalSystemAPI/service/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.project.VehicleRentalSystemAPI.dto.UserRegisterRequestDTO;
 import com.project.VehicleRentalSystemAPI.dto.UserRegisterResponseDTO;
+import com.project.VehicleRentalSystemAPI.exceptions.custom.*;
 import com.project.VehicleRentalSystemAPI.mappers.UserMapper;
 import com.project.VehicleRentalSystemAPI.model.entity.Users;
 import com.project.VehicleRentalSystemAPI.repository.UserRepository;
@@ -25,11 +26,11 @@ public class UserService {
 
         // Check's if the phone number or email already exists to throw an exception
         if (userRepository.existsByPhoneNumber(userRegisterRequestDTO.getPhoneNumber())) {
-            throw new IllegalArgumentException("Phone number already exists");
+            throw new PhoneNumberExistsException("Phone number already exists so use another");
         }
 
         if (userRepository.existsByEmail(userRegisterRequestDTO.getEmail())) {
-            throw new IllegalArgumentException("Email already exists");
+            throw new EmailExistsException("Email already exists so use another");
         }
 
         // Map's the request DTO to a User entity
